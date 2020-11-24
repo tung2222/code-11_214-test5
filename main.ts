40bda6eb88b907876e9f82a021c0411980246eba
tung2222 <71689384+tung2222@users.noreply.github.com>
2020-11-24 15:46:23 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.Silly)
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.No)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.Surprised)
+})
+basic.forever(function () {
+	
+})
